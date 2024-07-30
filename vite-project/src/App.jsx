3f635902c4208d379696a553a6d3f19f3a2071d8
DevRamona <r.ingabire@alustudent.com>
2024-07-30 20:11:21 +0200

    new

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,93 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Clients from "./Components/Clients"
-import Data from "./Components/Data"
-import ProjectImages from './Components/ProjectImages'
-import Projects from "./Components/Projects"
-import Features from './Components/Features'
-import FeaturesData from './Components/FeaturesData'
-import Services from './Components/Services'
-import servicesData from './Components/servicesData'
-import teamData from './Components/teamData'
-import Team from './Components/Team'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Clients from "./Components/Clients";
+import Data from "./Components/Data";
+import ProjectImages from "./Components/ProjectImages";
+import Projects from "./Components/Projects";
+import Features from "./Components/Features";
+import FeaturesData from "./Components/FeaturesData";
+import Services from "./Components/Services";
+import servicesData from "./Components/servicesData";
+import teamData from "./Components/teamData";
+import Team from "./Components/Team";
 function App() {
-  
-  
-  const client = Data.map((item) => 
-   (
-    <Clients
-    img = {item.img}
-    title = {item.title}
-    name = {item.Author} />
-  ))
+  const client = Data.map((item) => (
+    <Clients img={item.img} title={item.title} name={item.Author} />
+  ));
 
-  const projectTitle = ProjectImages.map((image) => 
-    (
-      <Projects
-      img = {image.img}/>
-    ))
+  const projectTitle = ProjectImages.map((image) => (
+    <Projects img={image.img} />
+  ));
 
-    const featuresInfo = FeaturesData.map(feature => (
-      <Features
-      img = {feature.img}
-      title = {feature.title}
-      paragraph = {feature.paragraph}/>
-    ))
+  const featuresInfo = FeaturesData.map((feature) => (
+    <Features
+      img={feature.img}
+      title={feature.title}
+      paragraph={feature.paragraph}
+    />
+  ));
 
-    const servicesInfo = servicesData.map(service => 
-      <Services
-      img = {service.img}
-      title = {service.title}
-      content = {service.content}/>
-    )
+  const servicesInfo = servicesData.map((service) => (
+    <Services
+      img={service.img}
+      title={service.title}
+      content={service.content}
+    />
+  ));
 
-    const teamInfo = teamData.map(team => 
-      <Team
-      img ={team.img}
-      name = {team.fullName}
-      role = {team.role}/>
-    )
+  const teamInfo = teamData.map((team) => (
+    <Team img={team.img} name={team.fullName} role={team.role} />
+  ));
   return (
     <div>
-       <h1 className=" text-3xl">WHAT OUR CLIENTS SAY</h1>
-       <div className='grid grid-cols-3 grid-rows-2 gap-3 mt-8'>
-       {client}
+      <h1 className=" text-3xl">WHAT OUR CLIENTS SAY</h1>
+      <div className="grid grid-cols-3 grid-rows-2 gap-3 mt-8">{client}</div>
+      <h1 className="m-8 text-3xl font-bold">Gallery</h1>
+      <p className="mb-16">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus
+        leonec.
+      </p>
+      <div className="grid grid-cols-3 grid-rows-3">{projectTitle}</div>
+      <div className="bg-gray-200 p-8">
+        <h1 className="text-3xl font-semibold mt-9">FEATURES</h1>
+        <div className="grid grid-cols-4 gap-10 mt-9 ">{featuresInfo}</div>
+      </div>
 
-       </div>
-       <h1 className='m-8 text-3xl font-bold'>Gallery</h1>
-       <p className='mb-16'>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
-       <div className='grid grid-cols-3 grid-rows-3'>
-        {projectTitle}
-
-
-       </div>
-       <div className='bg-gray-200 p-8'>
-       <h1 className='text-3xl font-semibold mt-9'>FEATURES</h1>
-       <div className='grid grid-cols-4 gap-10 mt-9 '>
-        {featuresInfo}
-       </div>
-       </div>
-       
-
-       <div className='bg-blue-500 mt-16 text-white'>
-       <h1 className='text-3xl font-bold mb-10'>OUR SERVICES</h1>
-       <p className='mb-24'>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
-       <div className='grid grid-cols-3 grid-rows-2 gap-8 p-20'>
-        {servicesInfo}
-
-       </div>
-
-       <div className='grid grid-cols-4'>
+      <div className="bg-blue-500 mt-16 text-white">
+        <h1 className="text-3xl font-bold mb-10">OUR SERVICES</h1>
+        <p className="mb-24">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+          dapibus leonec.
+        </p>
+        <div className="grid grid-cols-3 grid-rows-2 gap-8 p-20">
+          {servicesInfo}
+        </div>
+      </div>
+      <h1></h1>
+      <div className="mt-20 text-center">
+      <div className='grid grid-cols-4'>
         {teamInfo}
 
        </div>
-       
-       </div>
+
+      </div>
       
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite-project/src/Components/Team.jsx
+++ vite-project/src/Components/Team.jsx
@@ -1,7 +1,9 @@
-const Team = () => {
+const Team = (props) => {
     return (
         <div>
-            
+            <img src={props.img}/>
+            <h2>{props.name}</h2>
+            <h3>{props.role}</h3>
         </div>
 
     )
